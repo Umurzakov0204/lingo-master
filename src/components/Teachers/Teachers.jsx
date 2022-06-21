afefa5bc2b02f8ab6881baefdb3158a03245c829
Umurzakov0204 <tomthompson0222@gmail.com>
2022-06-21 19:36:28 +0500

    share

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -1,36 +1,83 @@
 import React from "react";
-// import 'antd/dist/antd.css';
 import './Teachers.css'
+import foto from '../../img/foto.png'
 
 const Teachers = () => {
     return (
         <>
-      <h1 id={'teachers'}>Teachers</h1> <br/>
-            <hr/>
-          <div className="container">
-              <div className="quote">
-                  <img src={'https://cdn.pixabay.com/photo/2014/03/25/16/54/user-297566_960_720.png'} alt=""/>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusantium minus, sapiente earum quaerat repellat?</p>
-                      <span>John Doe</span>
-              </div>
-              <div className="quote">
-                  <img src={'https://cdn.pixabay.com/photo/2014/03/25/16/54/user-297566_960_720.png'} alt=""/>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusantium minus, sapiente earum quaerat repellat?</p>
-                      <span>John Doe</span>
-              </div>
-              <div className="quote">
-                  <img src={'https://cdn.pixabay.com/photo/2014/03/25/16/54/user-297566_960_720.png'} alt=""/>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusantium minus, sapiente earum quaerat repellat?</p>
-                      <span>John Doe</span>
-              </div>
-              <div className="quote">
-                  <img src={'https://cdn.pixabay.com/photo/2014/03/25/16/54/user-297566_960_720.png'} alt=""/>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusantium minus, sapiente earum quaerat repellat?</p>
-                      <span>John Doe</span>
-              </div>
-          </div>
+            <h1>Teachers</h1>
+            <section>
+                <div className="cards__block">
+                    <div className="card">
+                        <div>
+                            <img src={foto} alt="" />
+                        </div>
+                        <div>
+                            <h2>Teacher Name</h2>                            <p>Compassionate and results-oriented elementary school teacher with more <br/>
+                                than 4 years of experience managing medium- and large-sized classrooms. <br/>
+                                Arizona State Educator License.</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div>
+                            <img src={foto} alt="" />
+                        </div>
+                        <div>
+                            <h2>Teacher Name</h2>                            <p>Compassionate and results-oriented elementary school teacher with more <br/>
+                            than 4 years of experience managing medium- and large-sized classrooms. <br/>
+                            Arizona State Educator License.</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div>
+                            <img src={foto} alt="" />
+                        </div>
+                        <div>
+                            <h2>Teacher Name</h2>                            <p>Compassionate and results-oriented elementary school teacher with more <br/>
+                            than 4 years of experience managing medium- and large-sized classrooms. <br/>
+                            Arizona State Educator License.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="cards__block">
+                    <div className="card">
+                        <div>
+                            <img src={foto} alt="" />
+                        </div>
+                        <div>
+                            <h2>Teacher Name</h2>                            <p>Compassionate and results-oriented elementary school teacher with more <br/>
+                            than 4 years of experience managing medium- and large-sized classrooms. <br/>
+                            Arizona State Educator License.</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div>
+                            <img src={foto} alt="" />
+                        </div>
+                        <div>
+                            <h2>Teacher Name</h2>                            <p>Compassionate and results-oriented elementary school teacher with more <br/>
+                            than 4 years of experience managing medium- and large-sized classrooms. <br/>
+                            Arizona State Educator License.</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div>
+                            <img src={foto} alt="" />
+                        </div>
+                        <div>
+                            <h2>Teacher Name</h2>                            <p>Compassionate and results-oriented elementary school teacher with more <br/>
+                            than 4 years of experience managing medium- and large-sized classrooms. <br/>
+                            Arizona State Educator License.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
-    );
+);
 }
 
 export default Teachers;
